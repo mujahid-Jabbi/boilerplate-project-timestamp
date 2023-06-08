3ab94d5d9e9458f869c42d07a7ca5c06cd5b7ac9
Mujahid <mujahidjabbimuhammad@gmail.com>
2023-06-08 13:53:55 +0100

    Fixed An empty date parameter should return the current time in a JSON object with a unix key and An empty date parameter should return the current time in a JSON onject with a utc key

--- index.js
+++ index.js
@@ -15,29 +15,64 @@ app.get("/", function (req, res) {
 });
 
 
-app.get('/api/:date?', (req, res) => {
-  const dateString = req.params.date
-  const dateStringRegex = /^[0-9]+$/
-  const containNumbers = dateStringRegex.test(dateString);
 
-  if(!NumbersOnly){
-    const unixTimestamp = Date.parse(dateString);
-    const utcDate = new Date(unixTimestamp).toUTCString()
-    unixTimestamp
-    ? res.json({"unix": unixTimestamp, "utc": UTCDate}): res.json({"error": "Invalid Date"})
-  }else{
-    const unixTimestamp = parseInt(dateString)
-    const actualDate = new Date(unixTimestamp)
-    const utcDate = actualDate.toUTCString();
-    res.json({"unix": unixTimestamp, "utc":UTCDate})
-  }
- }),
+// app.get('/api/:date?', (req, res) => {
+//   const dateString = req.params.date
+//   const dateStringRegex = /^[0-9]+$/
+//   const numbersOnly = dateStringRegex.test(dateString)
  
- app.get('/api/', (req, res) => {
-  const currentDate = new Date.toUTCString()
-  const currentUnix = Date.parse(currentDate)
-  res.json({"unix": currentUnix, "utc": currentDate})
- })
+//   if (!numbersOnly) {
+//     const unixTimestamp = Date.parse(dateString)
+//     const utcDate = new Date(unixTimestamp).toUTCString()
+ 
+//     unixTimestamp
+//     ? res.json({ unix: unixTimestamp, utc: utcDate })
+//     : res.json({ error: "Invalid Date" })
+//   } 
+//   else {
+//     const unixTimestamp = parseInt(dateString)
+//     const actualDate = new Date(unixTimestamp)
+//     const utcDate = actualDate.toUTCString()
+ 
+//     res.json({ unix: unixTimestamp, utc: utcDate })
+//   }
+ 
+//   app.get("/api", (req, res)=>{
+//     let date = new Date();
+//     let UTC = date.getTime();
+//     UTC = new Date(UTC);
+//     UTS = UTC.toUTCString();
+//     let UNIX = date.getTime();
+//     res.json({ unix: UNIX, utc: UTS });
+//   })
+//  })
+ 
+app.get("/api/:date?", function (req, res) {
+
+  if (req.params.date === '' || req.params.date === undefined) {
+    const current = new Date();
+    return res.json({
+      unix: current.getTime(),
+      utc: current.toUTCString(),
+    });
+  }
+  
+  const date = new Date(req.params.date);
+  if (date.toString() === 'Invalid Date') {
+    const date2 = new Date(parseInt(req.params.date, 10));
+    if (date2.toString() === 'Invalid Date') {
+      return res.json({ error : "Invalid Date" });
+    }
+    return res.json({
+      unix: date2.getTime(),
+      utc: date2.toUTCString(),
+    }); 
+  }
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString(),
+    });
+});
 
 
 
